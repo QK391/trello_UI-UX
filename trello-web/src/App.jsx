ee14c7b0b0ccb37e8485b360a1f43ae9ab81e173
QK391 <lexuanky391@gmail.com>
2024-07-03 23:17:36 +0700

    ABC

--- trello-web/src/App.jsx
+++ trello-web/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { useColorScheme } from "@mui/material/styles";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import ContrastIcon from "@mui/icons-material/Contrast";
+//import useMediaQuery from '@mui/material/useMediaQuery';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    const selectMode = event.target.value;
+    setMode(selectMode);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <LightModeIcon/>   
+        </MenuItem>
+        <MenuItem value="dark">
+          <DarkModeIcon/>
+        </MenuItem>
+        <MenuItem value="system">
+          <ContrastIcon/>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
 }
 
-export default App
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)')
+  // console.log('prefersDarkMode :', prefersDarkMode)
+  // console.log('prefersLightMode :', prefersLightMode)
+
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === "light" ? "dark" : "light");
+      }}
+    >
+      {mode === "light" ? "Turn dark" : "Turn light"}
+    </Button>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <ModeSelect />
+      <hr />
+      <ModeToggle />
+      <hr />
+      <div>Hello word</div>
+      <Typography variant="body2" component="text.secondary">
+        ABC
+      </Typography>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
