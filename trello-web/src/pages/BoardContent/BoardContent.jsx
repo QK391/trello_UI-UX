62f7cdf9552a52863bd35bb6f7800fcf025ce715
QK391 <lexuanky391@gmail.com>
2024-08-11 23:55:39 +0700

    Kéo thả Trello Card với Dnd-kit: onDragOver

--- trello-web/src/pages/BoardContent/BoardContent.jsx
+++ trello-web/src/pages/BoardContent/BoardContent.jsx
@@ -7,7 +7,6 @@ import {
   DndContext,
   useSensor,
   useSensors,
-  //PointerSensor,
   MouseSensor,
   TouchSensor,
   DragOverlay,
@@ -16,6 +15,8 @@ import {
 import Column from "./ListColumns/Columns/Column";
 import Card from "./ListColumns/Columns/ListCards/Card/Card";
 import { arrayMove } from "@dnd-kit/sortable";
+import { cloneDeep } from "lodash"
+
 const ACTIVE_DRAG_ITEM_TYPE = {
   COLUMN: "ACTIVE_DRAG_ITEM_TYPE_COLUMN",
   CARD: "ACTIVE_DRAG_ITEM_TYPE_CARD",
@@ -45,6 +46,13 @@ function BoardContent({ board }) {
     setOrderColumnState(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
   }, [board]);
 
+  //Timf column theo cardId
+  const findColumnByCardId = (cardId) => {
+    return orderebColumns.find((column) =>
+      column?.cards?.map((card) => card._id)?.includes(cardId)
+    );
+  };
+  //Triger kéo thả phần tử
   const handleDragStart = (event) => {
     // console.log("handleDragStart :", event);
     setActiveDragItemId(event?.active?.id);
@@ -59,8 +67,62 @@ function BoardContent({ board }) {
   console.log(activeDragItemType);
   console.log(activeDragItemData);
 
+  //Triger trong quá kéo thả phần tử
+  const handleDragOver = (event) => {
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) return;
+    console.log("handleDragOver :", event);
+    const { active, over } = event;
+    if (!active || !over) return;
+    const {
+      id: activeDringCardId,
+      data: { current: activeDringCardData },
+    } = active;
+    const { id: overCardId } = over;
+    //timf 2 column theo card
+    const activeColumn = findColumnByCardId(activeDringCardId);
+    const overColumn = findColumnByCardId(overCardId);
+
+    if (!activeColumn || !overColumn) return;
+    if (activeColumn._id !== overColumn._id) {
+      setOrderColumnState((prevColumns) => {
+        const overCardIndex = overColumn?.cards?.findIndex(
+          (card) => card._id === overCardId
+        );
+        let newCardIndex;
+        const isBelowOverItem =
+          active.rect.current.translated &&
+          active.rect.current.translated.top > over.rect.top + over.rect.height;
+        const modifier = isBelowOverItem ? 1 : 0;
+        newCardIndex = newCardIndex >= 0 ? newCardIndex + modifier : overColumn ?. cards ?. length + 1;
+
+        const nextColumns = cloneDeep[prevColumns]
+        const nextActiverColumn = nextColumns.find(column => column._id === activeColumn._id)
+        const nextoverColumn = nextColumns.find(column => column._id ===  overColumn._id)
+
+        if(nextActiverColumn){
+          nextActiverColumn.cards= nextActiverColumn.cards.filter(card => card._id !== activeDringCardId)
+          //Cap nhat lai mang
+          nextActiverColumn.cardOderIds = nextActiverColumn.cards.map(card => card._id)
+        }
+        if(nextoverColumn){
+          nextoverColumn.cards= nextoverColumn.cards.filter(card => card._id !== activeDringCardId)
+          nextoverColumn.cards= nextoverColumn.cards.toSpliced(newCardIndex, 0), activeDringCardData
+          nextoverColumn.cardOderIds = nextoverColumn.cards.map(card => card._id)
+
+        }
+        console.log('nextcolumn :', nextColumns)
+        return nextColumns;
+      })
+    }
+  };
+
   const handleDragEnd = (event) => {
-    console.log("handleDragEnd :", event);
+    // console.log("handleDragEnd :", event);
+
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      // console.log("kéo thả ko lm gì");
+      return;
+    }
     const { active, over } = event;
 
     if (!over) return;
@@ -96,8 +158,9 @@ function BoardContent({ board }) {
   };
   return (
     <DndContext
-      onDragEnd={handleDragEnd}
       sensors={sensors}
+      onDragEnd={handleDragEnd}
+      onDragOver={handleDragOver}
       onDragStart={handleDragStart}
     >
       <Box
@@ -109,7 +172,7 @@ function BoardContent({ board }) {
         }}
       >
         <ListColumns columns={orderebColumnsState} />
-        <DragOverlay dropAnimation = {dragAnimation}>
+        <DragOverlay dropAnimation={dragAnimation}>
           {!activeDragItemType && null}
           {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN && (
             <Column column={activeDragItemData} />
